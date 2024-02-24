98e0b77138052445ddbe8fc95aee037446a2e22d
rogerio-alvesf <rogeriobusiness@protonmail.com>
2024-02-24 12:48:59 -0300

    Modify: V1 compose funcionando.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 wait_time=15s
-password=YourStrong@Passw0rd
 
 # wait for SQL Server to come up
 echo importing data will start in $wait_time...
 sleep $wait_time
 echo executing script...
 
-# run the init script to create the DB and the tables in /table
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i ./DbObjects/setup.sql
+
+echo executing DbObjects/setup.sql
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i DbObjects/setup.sql
