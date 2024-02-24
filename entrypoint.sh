1f43db126888d6bef3ad22bb1b5266d2ba9f51ac
rogerio-alvesf <rogeriobusiness@protonmail.com>
2024-02-24 19:26:09 -0300

    Fix: ajuste no entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,5 @@ echo importing data will start in $wait_time...
 sleep $wait_time
 echo executing script...
 
-# run the scripts in /Schemas folder to create the DB and the tables
-for entry in "Schemas/*.sql"
-do
-  echo executing $entry
-  /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i $entry
-done
+echo executing Schemas/setup.sql
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $SA_PASSWORD -i Schemas/setup.sql
